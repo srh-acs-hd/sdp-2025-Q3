e65204b03f7551b2ad0ec7f21c41a4bc0450f4ac
srh-acs-hd <paul.tanzer@googlemail.com>
2025-10-23 15:09:56 +0200

    car example extended

--- examples/JavaExamples/src/oop/Car.java
+++ examples/JavaExamples/src/oop/Car.java
@@ -1,61 +1,81 @@
 package oop;
 
 public class Car {
-	private String brand;
-	private int doors;
-	private int speed = 0;
-	public static int createdCars = 0;
-	
-	public Car() {
-		System.out.println("new car is created");
-		this.brand="Unknown";
-		this.doors = 0;
-		this.speed = 0;
-		// static counter
-		Car.createdCars++;
-	}
-	
-	public void drive() {
-		System.out.println("Brumm");
-	}
-	
-	public void printDetails() {
-		System.out.println("Brand: "+brand+", Doors:"+doors+", Speed: "+speed);
-	}
-	
-	public void accelerate(int difference) {
-		speed = speed + difference;
-	}
+    private String brand;
+    private int doors;
+    private int speed = 0;
+    //private String owner;
+    private String ownerFirstname;
+    private String ownerLastname;
 
-	public int getSpeed() {
-		return speed;
-	}
+    public String getOwner() {
+        return this.ownerLastname + ", "+ this.ownerFirstname;
+    }
 
-	public void setSpeed(int speed) {
-		if (speed < 0) {
-			this.speed = 0;
-		}
-		else {
-			this.speed = speed;	
-		}
-		
-	}
+    public void setOwner(String owner) {
+        String[] nameValues = owner.split(" ");
+        this.ownerFirstname = nameValues[0];
+        this.ownerLastname = nameValues[1];
+    }
 
-	public String getBrand() {
-		return brand;
-	}
+    public Car() {
+        System.out.println("new car is created");
+        this.brand = "Unknown";
+        this.doors = 0;
+        this.speed = 0;
+        // static counter
+    }
 
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
+    public Car(String brand, int doors, int speed) {
+        this.brand = brand;
+        this.doors = doors;
+        this.speed = speed;
+    }
 
-	public int getDoors() {
-		return doors;
-	}
 
-	public void setDoors(int doors) {
-		this.doors = doors;
-	}
-	
-	
+    public void drive() {
+        System.out.println("Brumm");
+    }
+
+    public void printDetails() {
+        System.out.println("Brand: " + brand + ", Doors:" + doors + ", Speed: " + speed);
+    }
+
+    public void accelerate(int difference) {
+        speed = speed + difference;
+        if (speed < 0) {
+            speed = 0;
+        }
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed < 0) {
+            this.speed = 0;
+        } else {
+            this.speed = speed;
+        }
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+
 }
